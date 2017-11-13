eee97f0e788a8836f8285c0740eb9c922e5b423b
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2017-11-13 19:39:52 +0200

    am inceput device motion

--- js/senzori.js
+++ js/senzori.js
@@ -1,10 +1,19 @@
 
 window.addEventListener("deviceorientation", on_device_orientantion);
-
-
+window.addEventListener("deviceorientation", on_device_motion);
+//----------------------------------------
 function on_device_orientantion(e)
 {
 		document.getElementById("id_x").innerHTML = "x=" + e.beta;
 		document.getElementById("id_y").innerHTML = "y=" + e.gamma;
 		document.getElementById("id_z").innerHTML = "z=" + e.alpha;	
+}
+
+function on_device_motion(e)
+
+{
+	
+	
+	
+	
 }

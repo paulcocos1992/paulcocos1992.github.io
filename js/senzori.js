e174547722910b40b46d83c187994b285947ff58
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2017-11-13 19:12:44 +0200

    am citit senzori de orientare

--- js/senzori.js
+++ js/senzori.js
@@ -1,1 +1,10 @@
-document.getElementById("id_x").innerHTML = "SALUT";
+
+window.addEventListener("deviceorientation", on_device_orientantion);
+
+
+function on_device_orientantion(e)
+{
+		document.getElementById("id_x").innerHTML = e.beta;
+		document.getElementById("id_y").innerHTML = e.gamma;
+		document.getElementById("id_z").innerHTML = e.alpha;	
+}

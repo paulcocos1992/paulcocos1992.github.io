7837fd908a02c01160b329d38044ca0c931533ec
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2017-11-13 19:26:19 +0200

    am adaugat sir

--- js/senzori.js
+++ js/senzori.js
@@ -4,7 +4,7 @@ window.addEventListener("deviceorientation", on_device_orientantion);
 
 function on_device_orientantion(e)
 {
-		document.getElementById("id_x").innerHTML = e.beta;
-		document.getElementById("id_y").innerHTML = e.gamma;
-		document.getElementById("id_z").innerHTML = e.alpha;	
+		document.getElementById("id_x").innerHTML = "x=" + e.beta;
+		document.getElementById("id_y").innerHTML = "y=" + e.gamma;
+		document.getElementById("id_z").innerHTML = "z=" + e.alpha;	
 }

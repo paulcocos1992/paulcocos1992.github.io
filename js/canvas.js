43a525964bda7fe6749f286918642d8e25070440
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2017-12-04 18:14:00 +0200

    pamfil

--- js/canvas.js
+++ js/canvas.js
@@ -1,44 +1,41 @@
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.04.11";
+ //window.addEventListener("deviceorientation",on_device_orientation);// sa nu se deseneze de 2 ori cercul ---- Tot apare 
+ window.addEventListener("devicemotion",on_device_motion);
 
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.28.0";
-
-window.addEventListener("deviceorientation", on_device_orientation);
-//window.addEventListener("devicemotion", on_device_motion);
-
-var canvas = document.getElementById("id_canvas");
-var ctx = canvas.getContext("2d");
-
-setInterval(deseneaza_cerc, 40, gamma, beta);
-
-
-//-----------------------------------------------------
-function deseneaza_cerc();//(unghi1, unghi2)
+ //---------------------------------------------------------------
+ function deseneaza_cerc()//(unghi1,unghi2)
+ {
+	var canvas = document.getElementById("id_canvas");
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect(0,0,400,400);
+    ctx.rect(0,0,400,400);
+	ctx.stroke();
+	ctx.beginPath();
+	ctx.arc(200 + beta*200/90,200 + gamma*200/90,20,0,2*Math.PI,true);
+	ctx.fillStyle="#FF0000";//se umple cercul cu culoarea este rosu
+	//ctx.fill();
+	ctx.strokeStyle="#00FF00";
+	ctx.lineWidth=5;
+	ctx.stroke(); 
+ }
+ var gamma = 0;
+ var beta = 0;
+ setInterval(deseneaza_cerc,40);//,gamma,beta)
+ //----------------------------------------------------------------
+ function on_device_motion(e)
 {
-		ctx.clearRect(0, 0, 400, 400);
-		
-		ctx.rect(0, 0, 400, 400);
-		ctx.stroke();
-		
-		ctx.beginPath();
-		ctx.arc(200 + gamma * 200 / 90, 200 + beta * 200 / 90, 20, 0, 2 * Math.PI);
-		ctx.fillStyle = "#FF0000";
-		//ctx.fill();
-		ctx.strokeStyle = "#00FF00";
-		ctx.lineWidth = 5;
-		ctx.stroke();
-}
-//-----------------------------------------------------
-function on_device_orientation(e)
-{
-	deseneaza_cerc(e.gamma, e.beta);
-	gamma = e.gamma;
-	beta = e.beta;
+	 beta = - Math.atan(e.accelerationIncludingGravity.x/e.accelerationIncludingGravity.z)*180/Math.PI;
+	 gamma = Math.atan(e.accelerationIncludingGravity.y/e.accelerationIncludingGravity.z)*180/Math.PI;
+	
+	//deseneaza_cerc(beta,gamma);  //nu mai am nevoie de a desena cercul pt ca apelez functia de setInterval
 	
 }
-//-----------------------------------------------------
-function on_device_motion(e)
+ //--------------------------------------------
+ function on_device_orientation(e)
 {
-	var beta = Math.atan(e.accelerationIncludingGravity.y / e.accelerationIncludingGravity.z) * 180 / Math.PI;
-	var gamma = -Math.atan(e.accelerationIncludingGravity.x / e.accelerationIncludingGravity.z) * 180 / Math.PI;
+	deseneaza_cerc(e.gamma,e.beta);
 	
-	deseneaza_cerc(gamma, beta);
 }
+
+
+//

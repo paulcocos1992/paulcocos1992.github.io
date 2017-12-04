802aa9cf0a8a24fabba95d04ea76f00517724163
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2017-12-04 19:44:30 +0200

    SKSAKSA

--- js/touch.js
+++ js/touch.js
@@ -5,17 +5,20 @@ var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 canvas.adEventListener("touchstart", on_start_touch);
 
+var lista_culori = ["#FF000", "#00FF00", "#0000FF", "#FFF000", "#FF00FF", "#FFFFFF"];
+
+
 
 //-----------------------------------------
 function on_start_touch(e)
 {
 	var touches = e.changedTouches;
-	for (var i = 0; i < touches.lenght; i ++)
+	for (var i = 0; i < touches.lenght; i ++) {
 		context.beginPath();
 		context.arc(touches.item(i).pageX, touches.item(i).pageY, 20, 0, 2 * Math.PI);
 		context.stroke();
 		
-		
+	}
 }
  
 //-----------------------------------------

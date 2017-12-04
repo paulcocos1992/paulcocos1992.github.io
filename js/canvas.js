eded8a48f994c26ff888a31701ccaebf1471b5f6
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2017-12-04 17:53:31 +0200

    hello

---
+++ js/canvas.js
@@ -0,0 +1,44 @@
+[5:49 PM, 12/4/2017] Sandu: document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.28.0";
+
+window.addEventListener("deviceorientation", on_device_orientation);
+//window.addEventListener("devicemotion", on_device_motion);
+
+var canvas = document.getElementById("id_canvas");
+var ctx = canvas.getContext("2d");
+
+setInterval(deseneaza_cerc, 40, gamma, beta);
+
+
+//-----------------------------------------------------
+function deseneaza_cerc(unghi1, unghi2)
+{
+		ctx.clearRect(0, 0, 400, 400);
+		
+		ctx.rect(0, 0, 400, 400);
+		ctx.stroke();
+		
+		ctx.beginPath();
+		ctx.arc(200 + unghi1 * 200 / 90, 200 + unghi2 * 200 / 90, 20, 0, 2 * Math.PI);
+		ctx.fillStyle = "#FF0000";
+		//ctx.fill();
+		ctx.strokeStyle = "#00FF00";
+		ctx.lineWidth = 5;
+		ctx.stroke();
+}
+//-----------------------------------------------------
+function on_device_orientation(e)
+{
+	deseneaza_cerc(e.gamma, e.beta);
+	gamma = e.gamma;
+	beta = e.beta;
+	
+}
+//-----------------------------------------------------
+function on_device_motion(e)
+{
+	var beta = Math.atan(e.accelerationIncludingGravity.y / e.accelerationIncludingGravity.z) * 180 / Math.PI;
+	var gamma = -Math.atan(e.accelerationIncludingGravity.x / e.accelerationIncludingGravity.z) * 180 / Math.PI;
+	
+	deseneaza_cerc(gamma, beta);
+}
+[5:49 PM, 12/4/2017] Sandu: i senzori canvas

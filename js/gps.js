8f5c59320e3a790ce7594b10a97b22200199ca2d
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2017-12-14 19:10:48 +0200

    gps

---
+++ js/gps.js
@@ -0,0 +1,19 @@
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.0"
+navigator.geolocation.getCurrentPosition(on_position_succes, on_position_failure);
+
+
+//-----------------------------
+function on_position_succes(e)
+{
+	document.getElementById("id_lat").innerHTML =  e.coords.latitude;
+	document.getElementById("id_long").innerHTML =  e.coords.longitude;
+	
+}
+//-------------------------------
+function on_position_failure(e)
+{
+	alert("M-am pierdut!");
+	
+	
+}
+//-----------------------------

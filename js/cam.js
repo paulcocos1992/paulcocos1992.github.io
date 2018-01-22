30b63b8f04639f0641572a7224b23a75edfc6061
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2018-01-22 18:29:20 +0200

    asa

--- js/cam.js
+++ js/cam.js
@@ -1,9 +1,10 @@
 var d = new Date();
 document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
 //_______________________________________________###____________________________________________
-var constraints={audio: true,video: true}; {facingMode: "environment"};
+var constraints={audio: true,video: true}; {facingMode: "environment"}};
 navigator.mediaDevices.getUserMedia(constraints).then(on_cam).catch(on_error);
 var video = document.getElementById("id_video");
+video.addEventListener("touchstart", on_touch_video);
 
 //^^^^^^^^^^^^^^^^6
 function on_cam(stream)
@@ -16,3 +17,11 @@ function on_error(e)
 	alert("ERROR!: cannot connect to camera!");
 }
 //^^^^^^^^^^^^^^^^6
+function on_touch_video(e)
+{
+  var canvas = document.getElementById("id_img");
+var ctx = canvas.getContext("2d");  
+ctx.drawImage(video, 0, 0 );
+	
+	
+}

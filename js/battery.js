166474d79cd7782880f523adc3d8f584bc3b9d59
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2018-01-22 19:31:39 +0200

    bos

---
+++ js/battery.js
@@ -0,0 +1,16 @@
+var d = new Date ();
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
+d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
+
+
+navigator.getBattery().then(on_battery).catch(on_battery_error);
+//------------------------------
+function on_battery(b)
+{
+	document.getElementById("id_battery").innerHTML = b.level;
+}
+//------------------------------
+function on_battery_error()
+{
+	alert("Cannot read battery");
+}

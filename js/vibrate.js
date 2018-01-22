81cc2ea07ec6b02004e3fd9c92b7aac74479c1f8
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2018-01-22 18:58:18 +0200

    asa

--- js/vibrate.js
+++ js/vibrate.js
@@ -1,5 +1,5 @@
 var d = new Date ();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
 d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
 
 

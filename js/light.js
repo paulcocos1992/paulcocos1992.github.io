fd1deda155c51850a7a9237aeafe14af46a2f797
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2018-01-22 19:45:17 +0200

    light

---
+++ js/light.js
@@ -0,0 +1,21 @@
+var d = new Date();
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
+
+
+var sensor = new AmbientLightSensor();
+sensor.onreading = on_light_reading;
+sensor.onerror = on_light_error
+sensor.start();
+
+//------------------
+funtion on_light_reading ()
+{
+document.getElementById("id_light").innerHTML = sensor.illuminance;
+}
+
+funtion on_light_error ()
+{
+alert("cannot read light sensor");
+
+}
+//---------------

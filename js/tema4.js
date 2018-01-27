926ac54992c204a467c1d716c1398ba5fb372aca
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2018-01-27 15:40:01 +0200

    bat

--- js/tema4.js
+++ js/tema4.js
@@ -1,12 +1,30 @@
-var d = new Date();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0";
+var d = new Date ();
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
+d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
+
+
 var img = document.getElementById("id_img");
+img.addEventListener("touchstart", on_touch_start);
+img.addEventListener("touchend", on_touch_end);
 
 
+function on_touch_start(e)
+{
+	e.preventDefault();
+	timer_id = setInterval(start_vibrate, 100);
 
-navigator.getBattery().then(function(battery) {
-    
-   var time = battery.chargingTime;
+}
 
-   document.querySelector('#chargingTime').textContent = battery.chargingTime;
-});
+//-----------------------------------------------
+function start_vibrate()
+{
+	window.navigator.vibrate(150);
+	
+}
+//------------------------------------------
+function on_touch_end()
+{
+	e.preventDefault();
+	clearInterval(timer_id);
+}
+//-------------------------------------------

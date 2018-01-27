4ae36fb3a20c61ee6eb4ebf53025ac63f7913540
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2018-01-27 18:12:33 +0200

    22

--- js/tema4.js
+++ js/tema4.js
@@ -1,7 +1,7 @@
 var d = new Date ();
 document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
 d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
-
+navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 
 var img = document.getElementById("id_img");
 img.addEventListener("touchstart", on_touch_start);
@@ -28,3 +28,18 @@ function on_touch_end()
 	clearInterval(timer_id);
 }
 //-------------------------------------------
+function on_position_success(e) // functia care prezinta locatia utilizatorului
+{ 
+	lat = e.coords.latitude;
+	lon = e.coords.longitude;
+
+	  var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
+	  e.coords.latitude + "," + e.coords.longitude + "&zoom=15"+"&size=600x500"+
+"&key=AIzaSyDvoY0i_x0wXeE7vAOztYvmCzDIfEtzAR0"+
+"&markers=color:blue|label:Z|"+
+	  e.coords.latitude  + "," + e.coords.longitude 
+	  ;
+	document.getElementById("id_img").src=map_str;
+	document.getElementById("id_explicatie").innerHTML = "Afisarea locatiei utilizatorului.";
+	
+}

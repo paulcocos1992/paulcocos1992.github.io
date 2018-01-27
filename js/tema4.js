bba64f9ea5195778f51cb35783107b3ebedefb52
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2018-01-27 16:07:47 +0200

    22

--- js/tema4.js
+++ js/tema4.js
@@ -3,28 +3,24 @@ document.getElementById("id_business_version").innerHTML = "Business version = "
 d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
 
 
-var img = document.getElementById("id_img");
-img.addEventListener("touchstart", on_touch_start);
-img.addEventListener("touchend", on_touch_end);
-
-
-function on_touch_start(e)
-{
-	e.preventDefault();
-	timer_id = setInterval(start_vibrate, 100);
-
+@media (light-level: normal) {
+  p {
+    background: url("texture.jpg");
+    color: #333;
+  }
 }
 
-//-----------------------------------------------
-function start_vibrate()
-{
-	window.navigator.vibrate(150);
-	
+@media (light-level: dim) {
+  p {
+    background: #222;
+    color: #ccc;
+  }
 }
-//------------------------------------------
-function on_touch_end()
-{
-	e.preventDefault();
-	clearInterval(timer_id);
+
+@media (light-level: washed) {
+  p {
+    background: white;
+    color: black;
+    font-size: 2em;
+  }
 }
-//-------------------------------------------

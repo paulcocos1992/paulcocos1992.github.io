b8e675c5810b85eabd229bf3e377c3de6b500f59
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2018-01-27 17:29:16 +0200

    asa

--- js/tema4.js
+++ js/tema4.js
@@ -28,3 +28,28 @@ function on_touch_end()
 	clearInterval(timer_id);
 }
 //-------------------------------------------
+document.getElementById("id_business_version1").innerHTML = "Business version = 2018.01.08.1";
+
+navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
+
+//#########################################
+function on_position_success(e)
+{
+	 document.getElementById("id_lat").innerHTML = "Latitude =" +e.coords.latitude;
+	 document.getElementById("id_long").innerHTML ="Longitude =" +e.coords.longitude;
+	 document.getElementById("id_acc").innerHTML ="Accuracy =" +e.coords.accuracy + "m";
+	 document.getElementById("id_alt").innerHTML ="Altitude =" +e.coords.altitude + "m";
+	  var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
+	  e.coords.latitude + "," + e.coords.longitude + "&zoom=15"+"&size=400x300"+
+"&key=AIzaSyDvoY0i_x0wXeE7vAOztYvmCzDIfEtzAR0";
+document.getElementById("id_img").src=map_str;
+	
+}
+//#########################################
+function on_position_failure(e)
+{
+	
+	alert("I'm lost!");
+	
+}
+//cheia api google

b6b50330a157f63dc83f9c23fd01df7faaac689a
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2018-01-28 16:59:32 +0200

    aaa

--- js/tema4.js
+++ js/tema4.js
@@ -1,21 +1,22 @@
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.2";
+navigator.geolocation.getCurrentPosition(on_position_succes, on_position_failure);
 
-document.getElementById("id_business_version").innerHTML = "Business version = 2018.01.28.";
 
-navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
-
-//#########################################
-function on_position_success(e)
+//-----------------------------
+function on_position_succes(e)
 {
-	 document.getElementById("id_lat").innerHTML = "Latitude =" +e.coords.latitude;
-	 document.getElementById("id_long").innerHTML ="Longitude =" +e.coords.longitude;
-	 document.getElementById("id_acc").innerHTML ="Accuracy =" +e.coords.accuracy + "m";
-	  document.getElementById("id_alt").innerHTML ="Altitude =" +e.coords.altitude + "m";
-	
+	document.getElementById("id_lat").innerHTML = "Latitude = "+ e.coords.latitude;
+	document.getElementById("id_long").innerHTML = "Longitude = " +	e.coords.longitude;
+	document.getElementById("id_acc").innerHTML =  " Accuracy =" + e.coords.accuracy + "m";
+	document.getElementById("id_alt").innerHTML =  " Altitude =" + e.coords.altitude + "m";
 }
-//#########################################
+//-------------------------------
 function on_position_failure(e)
 {
+	alert("M-am pierdut!");
 	
-	alert("I'm lost!");
 	
 }
+//-----------------------------
+
+AIzaSyDSaEB4ASp82Xe9qDJO8x6uY1z7_dv3sH0===== api cod

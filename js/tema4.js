bf7216577d26aa271d8fcbc17ce292263dfe9966
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2018-01-28 16:55:56 +0200

    gggggggggggggggggggggggggggggggggg

--- js/tema4.js
+++ js/tema4.js
@@ -1,5 +1,21 @@
-jQuery(document).ready(function(){
-    jQuery('#hideshow').live('click', function(event) {        
-         jQuery('#content').toggle('show');
-    });
-});
+
+document.getElementById("id_business_version").innerHTML = "Business version = 2018.01.28.";
+
+navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
+
+//#########################################
+function on_position_success(e)
+{
+	 document.getElementById("id_lat").innerHTML = "Latitude =" +e.coords.latitude;
+	 document.getElementById("id_long").innerHTML ="Longitude =" +e.coords.longitude;
+	 document.getElementById("id_acc").innerHTML ="Accuracy =" +e.coords.accuracy + "m";
+	  document.getElementById("id_alt").innerHTML ="Altitude =" +e.coords.altitude + "m";
+	
+}
+//#########################################
+function on_position_failure(e)
+{
+	
+	alert("I'm lost!");
+	
+}

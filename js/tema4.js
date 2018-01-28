9a9f7a1bb0d8a418b52d78e5c547e207223c8764
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2018-01-28 16:20:45 +0200

    asa

--- js/tema4.js
+++ js/tema4.js
@@ -4,14 +4,22 @@ d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0";
 
 
 
+// Vibration for 50ms on all .button on click
+$(".button").vibrate();
 
-// Vibrate on completion
-var pattern = [500, 100, 500];
-$(".progress .bar")
-    .css({width: "0%"})
-    .animate({width: "100%"}, {
-        duration: 1000,
-        complete: function() {
-            if (canVibrate) navigator.vibrate(pattern);
-        }
-    });
+// Vibrate for 20ms on click
+$(".button").vibrate("short");
+
+// Vibrate for 50ms on click
+$(".button").vibrate("medium");
+$(".button").vibrate("default");
+$(".button").vibrate(50);
+
+// Vibrate for 100ms on click
+$(".button").vibrate("long");
+
+// Vibrate for 1000ms on touchstart. Stop vibrating on touchend.
+$(".button").vibrate({
+    duration: 1000,
+    trigger: "touchstart"
+});

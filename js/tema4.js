25a7e8fea66fb976a58a4e5969373c3301f04e05
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2018-01-28 16:41:45 +0200

    555

--- js/tema4.js
+++ js/tema4.js
@@ -1,6 +1,16 @@
 var d = new Date ();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
-d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".2"; 
 
-var img = document.getElementById("id_img");
-navigator.vibrate(1000);
+
+
+navigator.getBattery().then(on_battery).catch(on_battery_error);
+//------------------------------
+function on_battery(b)
+{
+document.getElementById("id_battery").innerHTML = b.level + " " + b.charging;
+}
+//------------------------------
+function on_battery_error()
+{
+	alert("Cannot read battery");
+}

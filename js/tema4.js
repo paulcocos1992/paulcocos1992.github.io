325f328eaed95fedeb78bf7a8fd716b7659cbdab
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2018-01-28 16:27:34 +0200

    a

--- js/tema4.js
+++ js/tema4.js
@@ -4,4 +4,4 @@ d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0";
 
 
 var img = document.getElementById("id_img");
-navigator.vibrate([125,75,125,275,200,275,125,75,125,275,200,600,200,600]);
+navigator.vibrate([100,30,100,30,100,200,200,30,200,30,200,200,100,30,100,30,100]);

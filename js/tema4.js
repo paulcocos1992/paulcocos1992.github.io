9cf1d2c40f5bcef3c61339c6d6754a7550553ab6
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2018-01-28 19:47:10 +0200

    dgfdg

--- js/tema4.js
+++ js/tema4.js
@@ -1,37 +1,9 @@
 
-var d = new Date ();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".4"; 
-d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
-//navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
-
-var speech = new webkitSpeechRecognition();
 var lat ;
-var lon ; 
-speech.onresult = on_speech_results;
-speech.onspeechend = on_speech_end;
-speech.lang="en-US";
+var lon ;
+var d = new Date ();
 
-//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-function recognize()
-{
-	if(speech.start())
-	{}//rezolvare eroare incercare multipla de pornire speech recognition
-}
-
-//####################################################
-
-function on_speech_end()
-{
-		speech.stop();
-	
-}
-//####################################################
-function on_speech_results(e)
-{
-	document.getElementById("id_speech").innerHTML=e.results[0][0].transcript;
-	line(); // functia care traseaza linia
-}
-//#########################################
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".6"; 
 
 function on_position_success(e) // functia care prezinta locatia utilizatorului
 { 
@@ -48,21 +20,6 @@ function on_position_success(e) // functia care prezinta locatia utilizatorului
 	document.getElementById("id_explicatie").innerHTML = "Afisarea locatiei utilizatorului.";
 	
 }
-//#########################################
-function line(e)
-{
-		var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
-	  lat + "," + lon + "&zoom=15"+"&size=600x500"+
-"&key=AIzaSyDvoY0i_x0wXeE7vAOztYvmCzDIfEtzAR0"+
-"&markers=color:blue|label:Z|"+
-	  lat  + "," + lon + 
-	  "&path=color:red|" + lat + "," + lon + "|" + document.getElementById("id_speech").innerHTML
-	  ;
-	document.getElementById("id_img").src=map_str;
-	document.getElementById("id_explicatie").innerHTML = "Trasarea liniei de conectare intre pozitia curentare si cea recunoscuta";
-		
-	
-}
 
 function on_position_failure(e)
 {
@@ -76,13 +33,7 @@ function emptyPos()
 	document.getElementById("id_img").src="";
 }
 
-var d = new Date ();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".5"; 
 
-
-
-//navigator.getBattery().then(on_battery).catch(on_battery_error);
-//------------------------------
 function on_battery(b)
 {
 		document.getElementById("batNu").checked=false;

2e3813b18f290a2c798cecc5e39e2aceb44ae059
IOAN-PAUL COCOS <33027948+paulcocos1992@users.noreply.github.com>
2018-01-28 18:29:25 +0200

    12

--- js/tema4.js
+++ js/tema4.js
@@ -1,28 +1,9 @@
 
 var d = new Date ();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".4"; 
 d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
 navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 
-var img = document.getElementById("id_img");
-img.addEventListener("touchstart", on_touch_start);
-img.addEventListener("touchend", on_touch_end);
-
-
-function on_touch_start(e)
-{
-	e.preventDefault();
-	timer_id = setInterval(start_vibrate, 100);
-
-}
-
-//-----------------------------------------------
-function start_vibrate()
-{
-	window.navigator.vibrate(150);
-	
-}
-//------------------------------------------
 var speech = new webkitSpeechRecognition();
 var lat ;
 var lon ; 
